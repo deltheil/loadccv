deltheil/loadccv

commit 7a08904c8dfcf04a3e572a1a054cb4b2d293a9a1

use official nn.SpatialCrossMapLRN
See:

* https://github.com/torch/nn/pull/413
* https://github.com/torch/cunn/pull/179

backend/cudnn.lua
@@ -1,4 +1,4 @@
-require 'cudnn'
+local cudnn = require 'cudnn'
 
 local create_conv = function(ni, no, kw, kh, dw, dh, zp, weight, bias)
   local padw, padh = zp, zp
@@ -20,10 +20,18 @@ local create_avg_pool = function(kw, kh, dw, dh)
   return cudnn.SpatialAveragePooling(kw, kh, dw, dh)
 end
 
+local create_cross_map_lrn = function(size, kappa, alpha, beta)
+  assert(cudnn.version >= 3000, 'cudnn v3 or higher is required')
+  return function(size, kappa, alpha, beta)
+    return cudnn.SpatialCrossMapLRN(size, alpha, beta, kappa)
+  end
+end
+
 return {
   cuda                       = function() return true end,
   SpatialConvolution         = create_conv,
   ReLU                       = create_relu,
   SpatialMaxPooling          = create_max_pool,
   SpatialAveragePooling      = create_avg_pool,
+  SpatialCrossMapLRN         = create_cross_map_lrn,
 }

cli.lua
@@ -6,8 +6,7 @@ Load a ccv (libccv.org) network in Torch7.
   -o        (default '.')    output directory
   --softmax                  append a soft-max module to the network
   --spatial                  use spatial convolutions for fully-connected layers
-  --package (default 'nn')   specific package for operations (nn | cunn | cudnn)
-  --lrn     (default 'nn')   package for Local Response Norm. (nn | inn | cudnn)
+  --backend (default 'nn')   specific backend for operations (nn | cunn | cudnn)
   --verbose                  print layers information
   <path>    (string)         path of the ccv network (sqlite3 file)
 ]])

init.lua
@@ -17,51 +17,15 @@ local CCV_TYPES = {
   'CCV_CONVNET_LOCAL_RESPONSE_NORM',
 }
 
-do
-  -- This is a skeleton module: it does NOT implement the normalization.
-  --
-  -- It should only be used as an intermediate to be converted into a module
-  -- with a real implementation like:
-  --   nn.CrossMapNormalization (fbcunn)
-  --   inn.SpatialCrossResponseNormalization (imagine-nn)
-  local LocalRespNorm, parent = torch.class(
-    'nn.LocalResponseNormalization', 'nn.Identity'
-  )
+-- Backend for main operations (convolution, pooling, etc)
+local _NN
 
-  function LocalRespNorm:__init(size, kappa, alpha, beta)
-    parent.__init(self)
-    self.size  = size
-    self.kappa = kappa
-    self.alpha = alpha
-    self.beta  = beta
-  end
-
-  function LocalRespNorm:updateOutput(input)
-    print("[WARNING] nn.LocalResponseNormalization: void implementation!")
-    return parent.updateOutput(self, input)
-  end
-
-  function LocalRespNorm:updateGradInput(input, gradOutput)
-    print("[WARNING] nn.LocalResponseNormalization: void implementation!")
-    return parent.updateGradInput(self, input, gradOutput)
-  end
-end
-
--- Packages for main operations (convolution, pooling, etc)
-local PKG
-local LRN
-
-local require_packages = function(pkg, lrn)
+local require_backend = function(pkg)
   assert(
     (pkg == 'nn') or (pkg == 'cunn') or (pkg == 'cudnn'),
-    'unsupported package `' .. pkg .. '`'
+    'unsupported backend `' .. pkg .. '`'
   )
-  assert(
-    (lrn == 'nn') or (lrn == 'inn') or (lrn == 'cudnn'),
-    'unsupported LRN package `' .. lrn .. '`'
-  )
-  PKG = require('loadccv.package.' .. pkg)
-  LRN = require('loadccv.lrn.' .. lrn)(PKG.cuda())
+  _NN = require('loadccv.backend.' .. pkg)
 end
 
 local noop = function() end
@@ -199,11 +163,11 @@ local load_conv = function(db, layer, branches)
   local weight, bias = load_conv_weights(db, layer, ni/#branches, no, kw, kh)
   local offset, length = 1, no/#branches
   for _,net in ipairs(branches) do
-    net:add(PKG.SpatialConvolution(
+    net:add(_NN.SpatialConvolution(
       ni/#branches, no/#branches, kw, kh, dw, dh, zp,
       weight:narrow(1, offset, length), bias:narrow(1, offset, length)
     ))
-    net:add(PKG.ReLU())
+    net:add(_NN.ReLU())
     offset = length + 1
   end
   return ni, no, kw, kh, dw, dh, zp
@@ -220,9 +184,9 @@ local load_pool = function(db, layer, branches)
       net:add(nn.SpatialZeroPadding(zp))
     end
     if CCV_TYPES[layer.type] == 'CCV_CONVNET_MAX_POOL' then
-      net:add(PKG.SpatialMaxPooling(kw, kh, dw, dh))
+      net:add(_NN.SpatialMaxPooling(kw, kh, dw, dh))
     else
-      net:add(PKG.SpatialAveragePooling(kw, kh, dw, dh))
+      net:add(_NN.SpatialAveragePooling(kw, kh, dw, dh))
     end
   end
   return kw, dw, zp
@@ -234,7 +198,7 @@ local load_lrn = function(db, layer, branches)
   local alpha = layer.output_alpha
   local beta  = layer.output_beta
   for _,net in ipairs(branches) do
-    net:add(LRN(size, kappa, alpha, beta))
+    net:add(_NN.SpatialCrossMapLRN(size, kappa, alpha, beta))
   end
   return size, kappa, alpha, beta
 end
@@ -265,11 +229,11 @@ local load_fc = function(db, layer, net, fc_num, spatial)
       ni = ni/(kw*kh)
     end
     local weight, bias = load_conv_weights(db, layer, ni, no, kw, kh)
-    net:add(PKG.SpatialConvolution(ni, no, kw, kh, dw, dh, zp, weight, bias))
+    net:add(_NN.SpatialConvolution(ni, no, kw, kh, dw, dh, zp, weight, bias))
   else
     if fc_num == 1 then
       -- re-interleave (DHW -> HWD) to fit with ccv fully-connected data layout
-      if PKG.cuda() then
+      if _NN.cuda() then
         net:add(nn.Transpose({2, 3}, {3, 4}))
       else
         net:add(nn.Transpose({1, 2}, {2, 3}))
@@ -285,7 +249,7 @@ local load_fc = function(db, layer, net, fc_num, spatial)
   local relu
   if layer.output_relu > 0 then
     relu = true
-    net:add(PKG.ReLU())
+    net:add(_NN.ReLU())
   else
     relu = false
   end
@@ -314,7 +278,7 @@ local split = function(layer)
   local offset, length = 1, layer.input_matrix_channels/parts
   for i=1,parts do
     local net = nn.Sequential()
-    if PKG.cuda() then
+    if _NN.cuda() then
       net:add(nn.Narrow(2, offset, length))
     else
       net:add(nn.Narrow(1, offset, length))
@@ -345,7 +309,7 @@ local load = function(filename, options)
   local log      = options.verbose and print or noop
   local input, num_output
 
-  require_packages(options.package or 'nn', options.lrn or 'nn')
+  require_backend(options.backend or 'nn')
 
   for layer in fetch_all_layers(db) do
     if layer.input_matrix_partition ~= #branches then
@@ -408,7 +372,7 @@ local load = function(filename, options)
     collectgarbage()
   end
 
-  if PKG.cuda() then
+  if _NN.cuda() then
     net:cuda()
   end
 
@@ -424,9 +388,9 @@ local load = function(filename, options)
     },
     mean = mean,
     num_output = num_output,
-    package = (options.package or 'nn'),
+    backend = (options.backend or 'nn'),
     lrn = (options.lrn or 'nn'),
-    cuda = PKG:cuda(),
+    cuda = _NN:cuda(),
   }
 
   return net, meta

lrn/cudnn.lua
@@ -1,11 +1,0 @@
-local cudnn = require 'cudnn'
-
-local init = function(cuda)
-  assert(cuda, 'cudnn SpatialCrossMapLRN has no CPU support')
-  assert(cudnn.version >= 3000, 'cudnn v3 or higher is required')
-  return function(size, kappa, alpha, beta)
-    return cudnn.SpatialCrossMapLRN(size, alpha, beta, kappa)
-  end
-end
-
-return init

backend/cunn.lua
@@ -19,10 +19,15 @@ local create_avg_pool = function(kw, kh, dw, dh)
   return nn.SpatialAveragePooling(kw, kh, dw, dh)
 end
 
+local create_cross_map_lrn = function(size, kappa, alpha, beta)
+  return nn.SpatialCrossMapLRN(size, alpha, beta, kappa)
+end
+
 return {
   cuda                       = function() return true end,
   SpatialConvolution         = create_conv,
   ReLU                       = create_relu,
   SpatialMaxPooling          = create_max_pool,
   SpatialAveragePooling      = create_avg_pool,
+  SpatialCrossMapLRN         = create_cross_map_lrn,
 }

backend/nn.lua
@@ -19,10 +19,15 @@ local create_avg_pool = function(kw, kh, dw, dh)
   return nn.SpatialAveragePooling(kw, kh, dw, dh)
 end
 
+local create_cross_map_lrn = function(size, kappa, alpha, beta)
+  return nn.SpatialCrossMapLRN(size, alpha, beta, kappa)
+end
+
 return {
   cuda                       = function() return false end,
   SpatialConvolution         = create_conv,
   ReLU                       = create_relu,
   SpatialMaxPooling          = create_max_pool,
   SpatialAveragePooling      = create_avg_pool,
+  SpatialCrossMapLRN         = create_cross_map_lrn,
 }

example/main.lua
@@ -43,18 +43,14 @@ w, h = meta.input.width, meta.input.height
 if meta.cuda then
   require 'cunn'
 end
-require(meta.package) -- e.g 'cudnn'
-require(meta.lrn)     -- e.g 'inn'
+require(meta.backend) -- e.g 'cudnn'
 
 if args.verbose then
   print(
     string.format("%-20s: %s", 'CUDA', meta.cuda and 'enabled' or 'disabled')
   )
   print(
-    string.format("%-20s: %s", 'backend', meta.package)
-  )
-  print(
-    string.format("%-20s: %s", 'local resp. norm.', meta.lrn)
+    string.format("%-20s: %s", 'backend', meta.backend)
   )
   print(
     string.format("%-20s: %dx%d", 'expected image size', w, h)

loadccv-scm-1.rockspec
@@ -25,12 +25,9 @@ build = {
    type = "builtin",
    modules = {
       ['loadccv.init']          = 'init.lua',
-      ['loadccv.package.nn']    = 'package/nn.lua',
-      ['loadccv.package.cunn']  = 'package/cunn.lua',
-      ['loadccv.package.cudnn'] = 'package/cudnn.lua',
-      ['loadccv.lrn.nn']        = 'lrn/nn.lua',
-      ['loadccv.lrn.inn']       = 'lrn/inn.lua',
-      ['loadccv.lrn.cudnn']     = 'lrn/cudnn.lua',
+      ['loadccv.backend.nn']    = 'backend/nn.lua',
+      ['loadccv.backend.cunn']  = 'backend/cunn.lua',
+      ['loadccv.backend.cudnn'] = 'backend/cudnn.lua',
       ['loadccv']               = 'util.c'
    },
    install = {

commit 58d14c442dedf3faa95b316fba27dd108b8f3808

fix issues introduced by 7a08904

backend/cudnn.lua
@@ -22,9 +22,7 @@ end
 
 local create_cross_map_lrn = function(size, kappa, alpha, beta)
   assert(cudnn.version >= 3000, 'cudnn v3 or higher is required')
-  return function(size, kappa, alpha, beta)
-    return cudnn.SpatialCrossMapLRN(size, alpha, beta, kappa)
-  end
+  return cudnn.SpatialCrossMapLRN(size, alpha, beta, kappa)
 end
 
 return {

cli.lua
@@ -15,8 +15,7 @@ assert(pl.path.isdir(args.o), args.o .. ' is not a directory')
 
 local opts = {
   spatial = args.spatial,
-  package = args.package,
-  lrn     = args.lrn,
+  backend = args.backend,
   verbose = args.verbose,
 }
 

init.lua
@@ -389,7 +389,6 @@ local load = function(filename, options)
     mean = mean,
     num_output = num_output,
     backend = (options.backend or 'nn'),
-    lrn = (options.lrn or 'nn'),
     cuda = _NN:cuda(),
   }
 